6f6be9f69ff9857b8d754d98be08dd7ca75ea5fe
phazedlite <phazedlite@VLaptop>
2012-03-10 14:00:32 -0800

    Added more vision stuff....

--- Friarbots 2012/src/org/team3309/subsystems/VisionSubsystem.java
+++ Friarbots 2012/src/org/team3309/subsystems/VisionSubsystem.java
@@ -1,13 +1,10 @@
 package org.team3309.subsystems;
 
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 import javax.microedition.io.Connector;
 import javax.microedition.io.SocketConnection;
 
-import org.json.me.JSONArray;
-import org.json.me.JSONException;
 import org.json.me.JSONObject;
 import org.team3309.VisionKeys;
 
@@ -18,12 +15,10 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class VisionSubsystem extends Subsystem {
 
 	private static VisionSubsystem instance 	= null;
-	private SocketConnection socket 	= null;
-	private BufferedReader in 			= null;
-
-	JSONObject data				= null;
-
-	boolean connected 			= false;
+	private SocketConnection socket 			= null;
+	private BufferedReader in 					= null;
+	JSONObject data								= null;
+	boolean connected 							= false;
 
 	public VisionSubsystem() {
 		try {
@@ -66,11 +61,11 @@ public class VisionSubsystem extends Subsystem {
 	}
 
 	public double getOffAngle(){
-		return data.optDouble(VisionKeys.OFF_ANGLE);
+		return data.optInt(VisionKeys.OFF_ANGLE);
 	}
 
 	public boolean canShoot(){
-		return false;
+		return data.optBoolean(VisionKeys.CAN_SHOOT);
 	}
 	
 }
